26822aa9aa861a260bdb2a9c1d42fa4f132d12e2
549531 <180322397+549531@users.noreply.github.com>
2024-12-03 13:16:44 +0100

    draw a square

--- src/fragment_shader.glsl
+++ src/fragment_shader.glsl
@@ -3,7 +3,12 @@
 in vec2 pos;     // position of the pixel, both x and y in range of [-1; 1]
 out vec4 color;  // the color of the pixel, RGBA
 
+float rectangle(vec2 pos, vec2 center, vec2 size) {
+	vec2 dist = abs(pos - center) - size;
+	return max(dist.x, dist.y);
+}
+
 void main() {
-	// TODO: draw something
-	color = vec4(pos, 0, 1);
+	float sdf = rectangle(pos, vec2(0), vec2(.25));
+	color = vec4(vec3(sdf <= 0), 1);
 }
